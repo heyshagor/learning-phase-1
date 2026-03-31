34fc4c343c29ffdf11c17427984ae0afe92ff67b
Md Shagor <itzshagor9@gmail.com>
2026-04-01 00:01:45 +0600

    fist python writing programe

---
+++ python/firstClass.py
@@ -0,0 +1,12 @@
+# variables 
+
+x = 5
+name = "John"
+# print(x)
+user = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# print("Hello, " + name)
+# print("Hello, I am " + user)
+
+print(f"Hello, I am {user} and I am {age + 1} years old!")
